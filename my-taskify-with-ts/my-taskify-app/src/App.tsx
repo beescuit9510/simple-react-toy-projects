07e5225db6065c5752cd65051e9dd49a8ca585cf
beescuit9510 <pengsoon20@gmail.com>
2022-04-21 22:04:22 +0900

    feat taskify app

--- my-taskify-with-ts/my-taskify-app/src/App.tsx
+++ my-taskify-with-ts/my-taskify-app/src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import InputField from './components/InputField';
 import TodoList from './components/TodoList';
 import { Todo } from './model';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 const App: FC = (): JSX.Element => {
   const [todo, setTodo] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -25,12 +27,50 @@ const App: FC = (): JSX.Element => {
 
   console.log(todos);
 
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add;
+    let active = todos;
+    let complete = completedTodos;
+
+    if (source.droppableId === 'TodoList') {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+    if (source.droppableId === 'TodoList') {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+
   return (
-    <div className='App'>
-      <span className='heading'>taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className='App'>
+        <span className='heading'>taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 };
 
--- my-taskify-with-ts/my-taskify-app/src/components/SingleTodo.tsx
+++ my-taskify-with-ts/my-taskify-app/src/components/SingleTodo.tsx
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from '../model';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
 import TodoList from './TodoList';
+import { Draggable } from 'react-beautiful-dnd';
 
 interface Props {
+  index: number;
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+const SingleTodo = ({ index, todo, todos, setTodos }: Props) => {
   const [edit, setEdit] = useState<boolean>(false);
 
   const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
 
   const handleDone = (id: number) => {
     setTodos(
@@ -26,44 +34,77 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     setTodos(todos.filter((todo) => !(todo.id == id)));
   };
 
+  const handleEdit = (event: React.FormEvent, id: number) => {
+    event.preventDefault();
+
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, todo: editTodo };
+        }
+        return { ...todo };
+      })
+    );
+
+    setEdit(false);
+  };
+
   return (
-    <form className='todos__single'>
-      {edit && <input />}
-      {!edit && todo.isDone && (
-        <s className='todos__single--text'>{todo.todo}</s>
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided, snapshot) => (
+        <form
+          className={`todos__single ${snapshot.isDragging ? 'drag' : ''}`}
+          onSubmit={(event) => {
+            handleEdit(event, todo.id);
+          }}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {edit && (
+            <input
+              value={editTodo}
+              onChange={(event) => setEditTodo(event.target.value)}
+              className='todos__single--text'
+            />
+          )}
+          {!edit && todo.isDone && (
+            <s className='todos__single--text'>{todo.todo}</s>
+          )}
+          {!edit && !todo.isDone && (
+            <span className='todos__single--text'>{todo.todo}</span>
+          )}
+          <div>
+            <span
+              className='icon'
+              onClick={() => {
+                if (!edit && !todo.isDone) {
+                  setEdit(!edit);
+                }
+              }}
+            >
+              <AiFillEdit />
+            </span>
+            <span
+              className='icon'
+              onClick={() => {
+                handleDelete(todo.id);
+              }}
+            >
+              <AiFillDelete />
+            </span>
+            <span
+              className='icon'
+              onClick={() => {
+                handleDone(todo.id);
+              }}
+            >
+              <MdDone />
+            </span>
+          </div>
+        </form>
       )}
-      {!edit && !todo.isDone && (
-        <span className='todos__single--text'>{todo.todo}</span>
-      )}
-      <div>
-        <span
-          className='icon'
-          onClick={() => {
-            if (!edit && !todo.isDone) {
-              setEdit(!edit);
-            }
-          }}
-        >
-          <AiFillEdit />
-        </span>
-        <span
-          className='icon'
-          onClick={() => {
-            handleDelete(todo.id);
-          }}
-        >
-          <AiFillDelete />
-        </span>
-        <span
-          className='icon'
-          onClick={() => {
-            handleDone(todo.id);
-          }}
-        >
-          <MdDone />
-        </span>
-      </div>
-    </form>
+    </Draggable>
   );
 };
 
